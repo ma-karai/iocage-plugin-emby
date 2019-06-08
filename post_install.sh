11c162068643a9aa93d6f7d08d0a254283885c04
makarai <17119942+ma-karai@users.noreply.github.com>
2019-06-08 11:46:09 +0200

    update to 4.2.0.14

--- post_install.sh
+++ post_install.sh
@@ -5,7 +5,7 @@
 #pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/3.6.0.75/emby-server-freebsd_3.6.0.75_amd64.txz
 #pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/4.1.0.10/emby-server-freebsd_4.1.0.10_amd64.txz
 
-pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/4.2.0.11/emby-server-freebsd11_4.2.0.11_amd64.txz
+pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/4.2.0.14/emby-server-freebsd11_4.2.0.14_amd64.txz
 
 # Enable the service
 sysrc -f /etc/rc.conf emby_server_enable="YES"
